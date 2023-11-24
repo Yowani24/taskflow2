79318beaffbbcaefd912d5511769285fc2a46592
joao da costa <yowani24@outlook.com>
2023-11-24 10:36:28 -0300

    Feat: reposicionamento do modal de detalhes

--- src/components/taskFlowApp/TaskDetails.jsx
+++ src/components/taskFlowApp/TaskDetails.jsx
@@ -51,7 +51,7 @@ export function TaskDetails({ taskData, taskName, status }) {
       >
         <FaUpRightAndDownLeftFromCenter size={15} />
       </div>
-      <Dialog open={open} size="sm" className="-mt-14 md:m-0">
+      <Dialog open={open} size="sm" className=" xs:-mt-0 sm:-mt-14 md:m-0">
         <div className="flex items-center justify-between">
           <DialogHeader className="flex flex-col items-start">
             {" "}
