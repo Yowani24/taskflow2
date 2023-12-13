f871d99632f5dabd534ff2edf6ba79752d7b9318
joao da costa <yowani24@outlook.com>
2023-12-13 16:12:23 -0300

    Feat: Login and Register responsive

--- src/components/TopNavBar.tsx
+++ src/components/TopNavBar.tsx
@@ -11,6 +11,7 @@ import {
   Avatar,
   Chip,
 } from "@material-tailwind/react";
+import { FaUserCircle } from "react-icons/fa";
 
 import { RxDashboard } from "react-icons/rx";
 import { FaLanguage } from "react-icons/fa6";
@@ -147,6 +148,7 @@ export default function TopNavBar({ getCurrentTab }) {
                 className="cursor-pointer w-8 h-8 md:w-10 md:h-10"
                 src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
               />
+              {/* <FaUserCircle /> */}
             </MenuHandler>
             <MenuList>
               <MenuItem className="flex items-center gap-2 bg-white">
